49ab50e6953af401318c5cfbe50b70e23a5d0fc1
Maxim Surin <surin.maks@yandex.ru>
2025-05-14 14:11:08 +0300

    add 1192

---
+++ 1192/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip> 
+namespace ex1192{
+    class Solution
+    {
+    private:
+        int V_{0}, a_deg_{0};
+        double K_{1.0};
+        const double g_ = 10.0;
+        const double pi_ = 3.1415926535;
+    public:
+        void input();
+        void result();
+    };
+
+    void Solution::input(){
+        std::cin >> V_ >> a_deg_ >> K_;
+    }
+
+    void Solution::result(){
+        double a_rad = a_deg_ * pi_ / 180.0;
+        double sin_2alpha = std::sin(2 * a_rad);
+        double S1 = (V_ * V_) * sin_2alpha / g_;
+        double total_distance = S1 * K_ / (K_ - 1);
+        std::cout << std::fixed << std::setprecision(2) << total_distance;
+    }
+}
+
+int main(int, char**){
+    ex1192::Solution solution;
+    solution.input();
+    solution.result();
+}
