bc5e0104ee2f64313d34d1f66f6a1850ff3eb359
Maxim Surin <surin.maks@yandex.ru>
2025-05-20 09:02:59 +0300

    добавлены классы

--- 1521/main.cpp
+++ 1521/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 
+/**
+ * @brief Adstract class
+ */
+namespace ISolution{
+    class ISolution
+    {
+        public:
+        void virtual input() = 0;
+        void virtual result() = 0;
+        virtual ~ISolution() = default;
+    };
+}
+
+namespace ex1521{
+    class Solution : ISolution::ISolution
+    {
+        private:
+        public:
+            void input() override;
+            void result() override;
+    };
+}
+
 int main(int, char**){
-    std::cout << "Hello, from 1521!\n";
 }
